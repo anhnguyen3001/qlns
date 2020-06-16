f198f1d0685413098c7ce573c3258cc1e7957a6e
anhnguyen3001 <57490281+anhnguyen3001@users.noreply.github.com>
2020-06-16 16:17:49 +0700

    Update Employee.php

--- mvc/controllers/Employee.php
+++ mvc/controllers/Employee.php
@@ -74,9 +74,9 @@
                     if (isset($_POST['edit-info'])){
                         unset($_POST['edit-info']);
                         // $this->validation->validate($_POST);
-                        foreach($_POST as $key => $value){
-                            $_POST[$key] = $this->validation->formatData($key, $value);
-                        }
+                        // foreach($_POST as $key => $value){
+                            $_POST['gender'] = $this->validation->formatData('gender', $_POST['gender']);
+                        // }
 
                         // $_POST = $this->validation->validate($_POST);
                         print_r($_POST);
